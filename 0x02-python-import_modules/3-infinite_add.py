12323ca80fb61b4c5fbdb94f09b9664dafb19167
LinMihigo <linmihigo@gmail.com>
2024-09-12 22:01:56 +0200

    3-infinite_add.py (Fix)

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-len = len(sys.argv)
-result = 0
-for i in range(len):
-    if i > 0:
-        result += int(sys.argv[i])
-print(result)
+if __name__ == "__main__":
+    import sys
+    len = len(sys.argv)
+    result = 0
+    for i in range(len):
+        if i > 0:
+            result += int(sys.argv[i])
+    print("{}".format(result))
