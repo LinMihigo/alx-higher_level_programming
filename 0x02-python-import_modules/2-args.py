bfabe016eb836c218b2a2f076e6fc639135be0af
LinMihigo <linmihigo@gmail.com>
2024-09-12 21:40:06 +0200

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    print("{} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) > 0:
+        for i in range(len(sys.argv)):
+            if i > 0:
+                print("{}: {}".format(i, sys.argv[i]))
