8365cce03b0dc8075a58c6df2344de1ec91062e4
LinMihigo <linmihigo@gmail.com>
2024-09-18 23:14:45 +0200

    10-best_score.py (fix)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
-        return None
-    maxi = max(a_dictionary.values())
-    for k, v in a_dictionary.items():
-        if v == maxi:
-            return k
+    if a_dictionary:
+        for k, v in a_dictionary.items():
+            if v == max(a_dictionary.values()):
+                return k
+    return None
