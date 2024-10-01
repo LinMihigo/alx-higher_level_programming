bef87d2a7c59e58d0d3c18a6d7c44c48143f5a87
LinMihigo <linmihigo@gmail.com>
2024-10-01 20:52:37 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Defining a Square class. """
+
+
+class Square:
+    """ Description of the Square class
+
+
+    args:
+        size (int): size to set
+
+    Attributes:
+        __size (int): private instance attribute to be set.
+
+    """
+    def __init__(self, size):
+        self.__size = size
