21dfed01b7ee67706f7843837864f94b9d829d2a
LinMihigo <linmihigo@gmail.com>
2024-10-01 17:28:24 +0200

    0-square.py (6th fix)

--- 0x06-python_classes/0-square.py
+++ 0x06-python_classes/0-square.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
-
-"""Define a class Square."""
-
-
-=======
-#!/usr/bin/python3.8
 """ Defining the class Square. """
->>>>>>> parent of 4038c55... 0-square.py (3rd fix)
 class Square:
     """Represent a square."""
     pass
