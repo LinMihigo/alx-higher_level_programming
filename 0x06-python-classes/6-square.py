09421dcf9390444420762c08380e793acf057573
LinMihigo <linmihigo@gmail.com>
2024-10-02 14:24:29 +0200

    6-square.py (fix: property setter)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,12 +44,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (len(value) == 2 and isinstance(value, tuple) and
-            isinstance(value[0], int) and isinstance(value[1], int) and
-            value[0] > 0 and value[1] > 0):
-            self.__position = value
-        else:
+        if (len(value) != 2 or not isinstance(value, tuple) or
+            not isinstance(value[0], int) or not isinstance(value[1], int) or
+            value[0] > 0 or value[1] > 0):
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):  #: Calculates the square area
         return self.__size * self.__size
