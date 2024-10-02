7c9008a9621df408b67d60fa56c5439025b540c8
LinMihigo <linmihigo@gmail.com>
2024-10-02 18:31:57 +0200

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,17 @@
+import sys
+
+def safe_function(fct, *args):
+    """Executes a function safely, handling any exceptions.
+
+    Args:
+        fct: A pointer to the function to execute.
+        *args: Arguments to pass to the function.
+
+    Returns:
+        The result of the function execution if successful, otherwise None.
+    """
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
