c0aea976bb22e94cb56681f2c86096f64f9e4bdf
LinMihigo <linmihigo@gmail.com>
2024-10-20 10:07:29 +0200

    Almost_a_circle (2nd commit)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,42 @@ class Rectangle(Base):
 
     def area(self):
         return self.__width * self.__height
+
+    def display(self):
+        for k in range(self.__y):
+            print("")
+        for i in range(self.__height):
+            for l in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height})"
+    
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            self.id = args[0]
+        elif 'id' in kwargs:
+            self.id = kwargs['id']
+
+        if len(args) > 1:
+            self.__width = args[1]
+        elif 'width' in kwargs:
+            self.__width = kwargs['width']
+
+        if len(args) > 2:
+            self.__height = args[2]
+        elif 'height' in kwargs:
+            self.__height = kwargs['height']
+
+        if len(args) > 3:
+            self.__x = args[3]
+        elif 'x' in kwargs:
+            self.__x = kwargs['x']
+
+        if len(args) > 4:
+            self.__y = args[4]
+        elif 'y' in kwargs:
+            self.__y = kwargs['y']
