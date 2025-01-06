4bfa5c380019be871ae6615b3ac515efccc14e31
LinMihigo <linmihigo@gmail.com>
2025-01-06 22:13:41 +0200

    Task 16 (5th requirement fix)

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
 """ prints the State object with the name passed as argument from the database
 """
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-from relationship_state import State
+import sys
+from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
-              .format(argv[0]))
-        exit(1)
-
-    username, password, db_name = argv[1], argv[2], argv[3]
-
-    engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
-        pool_pre_ping=True
-        )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).order_by(State.id, City.id).all()
-
-    for state in states:
-        for city in state.cities:
-            print(f"{state.id}: {state.name} -> {city.id}: {city.name}")
-
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
